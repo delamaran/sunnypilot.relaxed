fffb10bb3469e8322124f9686ff5f1aab22ecc74
delamaran <ridiculousAddress@hotmail.com>
2026-04-19 19:34:55 -0700

    2026  04-19  Update helpers.py   Increased distracted warning time

--- selfdrive/monitoring/helpers.py
+++ selfdrive/monitoring/helpers.py
@@ -24,12 +24,12 @@ class DRIVER_MONITOR_SETTINGS:
   def __init__(self, device_type):
     self._DT_DMON = DT_DMON
     # ref (page15-16): https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:42018X1947&rid=2
-    self._AWARENESS_TIME = 30. # passive wheeltouch total timeout
-    self._AWARENESS_PRE_TIME_TILL_TERMINAL = 15.
-    self._AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.
-    self._DISTRACTED_TIME = 11. # active monitoring total timeout
-    self._DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
-    self._DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
+    self._AWARENESS_TIME = 999. # passive wheeltouch total timeout   CHANGED FROM 30 TO 90 TO 999
+    self._AWARENESS_PRE_TIME_TILL_TERMINAL =777.                 #   CHANGED FROM 15 TO 45 TO 777
+    self._AWARENESS_PROMPT_TIME_TILL_TERMINAL = 222.             #   CHANGED FROM  6 TO 18 TO 222
+    self._DISTRACTED_TIME = 999. # active monitoring total timeout   CHANGED FROM 11 TO 99 TO 999
+    self._DISTRACTED_PRE_TIME_TILL_TERMINAL = 777.               #   CHANGED FROM  8 TO 72 TO 777
+    self._DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 222.            #   CHANGED FROM  6 TO 54 TO 222
 
     self._FACE_THRESHOLD = 0.7
     self._EYE_THRESHOLD = 0.5
